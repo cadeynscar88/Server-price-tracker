38e2899e523a0a60b71724d49d8fd15cb3f07da9
cadeynscar88 <cadeynscar88@gmail.com>
2026-08-28 21:33:18 -0500

    Refresh PWA shell for quota counter

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Minimal service worker: cache app shell, network-first for tracker data.
-const SHELL = "pst-shell-v3";
+const SHELL = "pst-shell-v4";
 const SHELL_FILES = ["./", "./index.html", "./manifest.webmanifest"];
 self.addEventListener("install", e => {e.waitUntil(caches.open(SHELL).then(c=>c.addAll(SHELL_FILES)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate", e => {e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==SHELL).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
